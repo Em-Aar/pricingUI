2affc5bf65370ce189a9489891d8034e9ce5b160
Em-Aar <mmrhaq@gmail.com>
2023-12-30 20:57:07 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+
+    <section className="flex flex-col items-center ">
+      {/* TOP SECTION  */}
+      <div className="bg-[#6B46C1] h-[394px] text-gray-50 pt-[88px] lg:text-center pl-6 w-full">
+        <h3 className="text-3xl lg:text-5xl leading-10 font-extrabold mb-5">
+          Simple pricing for your business
+        </h3>
+        <p className="text-xl font-medium opacity[84]">
+          Plans that are carefully crafted to suit your business.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+      </div>
+
+      {/* MIDDLE SECTION */}
+
+      <div className="absolute top-[254px] mx-6 lg:mx-auto max-w-5xl  ">
+        {/* First Section */}
+        <div className="flex flex-col lg:flex-row lg:justify-center mx-auto shadow-xl">
+        <div className="pt-9 px-12 bg-[#F0EAFB] min-w-[382px] text-center rounded-t-lg lg:rounded-l-lg lg:rounded-tr-none ">
+          <p className="text-2xl font-extrabold text-gray-900">Premium PRO</p>
+          <span className="block mt-4 mb-3 text-5xl font-extrabold">$329</span>
+          <p className="mb-8 text-gray-900 text-lg font-medium">
+            billed just once
+          </p>
+          <button className="bg-[#805AD5] text-gray-50 px-[75px] py-3 rounded-lg mb-7">
+            Get Started
+          </button>
+        </div>
+
+        {/* Second Section */}
+        <div className="px-4 bg-white shadow-lg lg:rounded-r-lg rounded-b-lg ">
+          <p className="mb-6 mt-10">
+            Access these features when you get this pricing package for your
+            business.
+          </p>
+
+          <div className="flex justify-start  gap-x-3 gap-y-4 ">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={"/Group 172.svg"}
+              width={22}
+              height={22}
+              alt="price-image"
             />
-          </a>
+            <span className="text-lg text-gray-700">
+              International calling and messaging API
+            </span>
+          </div>
+
+          <div className="flex justify-start  gap-x-3 gap-y-4 ">
+            <Image
+              src={"/Group 172.svg"}
+              width={22}
+              height={22}
+              alt="price-image"
+            />
+            <span className="text-lg text-gray-700">
+              International calling and messaging API
+            </span>
+          </div>
+
+          <div className="flex justify-start  gap-x-3 gap-y-4 ">
+            <Image
+              src={"/Group 172.svg"}
+              width={22}
+              height={22}
+              alt="price-image"
+            />
+            <span className="text-lg text-gray-700">
+              International calling and messaging API
+            </span>
+          </div>
+
+          <div className="flex justify-start  gap-x-3 gap-y-4 mb-10">
+            <Image
+              src={"/Group 172.svg"}
+              width={22}
+              height={22}
+              alt="price-image"
+            />
+            <span className="text-lg text-gray-700">
+              International calling and messaging API
+            </span>
+          </div>
+        </div>
+        </div>
+
+        {/* Bottom Section */}
+
+        <div className="pl-2 lg:flex lg:justify-center lg:gap-x-6">
+          <div className="mt-8 flex justify-start  gap-x-2 gap-y-10">
+            <Image
+              src={"/Group 178.svg"}
+              width={33}
+              height={33}
+              alt="price-image"
+            />
+            <p>30 days money back Guarantree</p>
+          </div>
+          <div className="mt-8 flex justify-start  gap-x-2 gap-y-10">
+            <Image
+              src={"/Group 178.svg"}
+              width={33}
+              height={33}
+              alt="price-image"
+            />
+            <p>30 days money back Guarantree</p>
+          </div>
+          <div className="mt-8 flex justify-start  gap-x-2 gap-y-10">
+            <Image
+              src={"/Group 178.svg"}
+              width={33}
+              height={33}
+              alt="price-image"
+            />
+            <p>30 days money back Guarantree</p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </section>
+  );
 }
